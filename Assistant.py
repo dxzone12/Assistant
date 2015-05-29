f932cf4308ca03ebbcbe356a023844f039513865
Sam Lewis <Samuel-Lewis@users.noreply.github.com>
2015-05-30 00:49:58 +0930

    Custom layout
    Users have to predefine their own layouts, and then manually change the plugin to call that layout.
    In the future, would be good to detect current layout, and try and match it.
    Pretty shotty implementation though...

--- Assistant.py
+++ Assistant.py
@@ -8,6 +8,8 @@ source files will be to the left
 class AssistantListener(sublime_plugin.EventListener):
     def on_activated(self, view):
         file_name = view.file_name()
+        # custome layout name
+        layout_pref = "2cols"
 
         if file_name != None:
 
@@ -15,7 +17,7 @@ class AssistantListener(sublime_plugin.EventListener):
             if file_name.endswith(".cpp"):
                 header = view.window().find_open_file(file_name.replace('.cpp', '.h'))
                 if header != None:
-                    view.window().set_layout({"cols": [0, 0.5, 1], "rows": [0, 1], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+                    layout_panes(layout_pref)
                     view.window().set_view_index(header, 1, 0)
                     view.window().set_view_index(view, 0, view.window().get_view_index(view)[1])
 
@@ -23,6 +25,16 @@ class AssistantListener(sublime_plugin.EventListener):
             if file_name.endswith(".h"):
                 source = view.window().find_open_file(file_name.replace('.h', '.cpp'))
                 if source != None:
-                    view.window().set_layout({"cols": [0, 0.5, 1], "rows": [0, 1], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+                    layout_panes(layout_pref)
                     view.window().set_view_index(source, 0, 0)
                     view.window().set_view_index(view, 1, view.window().get_view_index(view)[1])
+
+    def layout_panes(layout)
+    # add custom layouts here.
+        if layout = "2cols":
+            view.window().set_layout({"cols": [0, 0.5, 1], "rows": [0, 1], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+        elif layout = "2rows":
+            view.window().set_layout({"cols": [0, 1], "rows": [0, 0.5, 1], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
+        else:
+            # default to 'rows' layout
+            view.window().set_layout({"cols": [0, 0.5, 1], "rows": [0, 1], "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]})
